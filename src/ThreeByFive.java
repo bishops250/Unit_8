01e11867dd4337bbe6631c319b7d9987a7ec8bee
bishops250 <bishops250@gmail.com>
2020-03-10 15:12:03 -0400

    Working on this...

--- src/ThreeByFive.java
+++ src/ThreeByFive.java
@@ -2,23 +2,49 @@ public class ThreeByFive {
     public static void main(String [] args) {
 // note...this is a 2x5 int array
 // a 3x5 array is required
-        int[][] nums = { {3,6,2,9,7}, {7,45,9,12,44} };
+        int[][] nums = { {3,6,2,9,7}, {7,45,9,12,44} ,{3,2,1,1,4}};
 
 //this output is incomplete for this array
         System.out.println(nums[0][0]);
+        System.out.println(nums[0][1]);
+        System.out.println(nums[0][2]);
         System.out.println(nums[0][3]);
+        System.out.println(nums[0][4]);
+
+
+        System.out.println(nums[1][0]);
         System.out.println(nums[1][1]);
+        System.out.println(nums[1][2]);
         System.out.println(nums[1][3]);
         System.out.println(nums[1][4]);
 
+        System.out.println(nums[2][0]);
+        System.out.println(nums[2][1]);
+        System.out.println(nums[2][2]);
+        System.out.println(nums[2][3]);
+        System.out.println(nums[2][4]);
+
+
+
 //change three other values to negative numbers
         nums[1][4] = -4;
+        nums[0][0] = -3;
+        nums[0][1] = -6;
+        nums[0][2] = -2;
 
 //make a statement to create a blank line here
+        System.out.println("");
 
 // output all of the numbers again,
 // not just the changed ones
         System.out.println(nums[1][4]);
+
+        for(int row=0; row<nums[0].length;row++) {
+            System.out.println(nums[row]);
+            for(int col=0; col<nums.length;col++) {
+
+            }
+        }
     }
 }
 
