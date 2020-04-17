adb2c7b0f46b3de04021d2e3df8ec583c680bea9
bishops250 <biishops250@gmail.com>
2020-04-17 00:02:36 -0400

    Done.

--- src/TokenPass.java
+++ src/TokenPass.java
@@ -9,12 +9,12 @@ public class TokenPass {
      * @param playerCount the number of players
      */
     public TokenPass(int playerCount) {
-        this.board= new int[playerCount];
+        this.board= new int[playerCount]; //creates array of desired size. Will contain the tokens
         for(int i=0;i<board.length;i++) {
-            board[i]=(int)(Math.random()*10+1);
+            board[i]=(int)(Math.random()*10+1); //sets each array to a random number
 
         }
-        currentPlayer=(int)(Math.random()*playerCount-1);
+        currentPlayer=(int)(Math.random()*playerCount-1); //chooses the starting player
 
     }
 
@@ -27,15 +27,38 @@ public class TokenPass {
      */
     public void distributeCurrentPlayerTokens() {
 
-        int currentPlayerToken=board[currentPlayer];
-        board[currentPlayer]=0;
-        int nextPlayer=currentPlayer;
+        int currentPlayerToken=board[currentPlayer]; /*sets distributing tokens to randomly generated
+        number in the array*/
+        board[currentPlayer]=0; //sets the tokens at that index to zero.
+        int nextPlayer=currentPlayer; //sets the next player to start at the current player
 
-        while (currentPlayerToken>0) {
-            nextPlayer%=board.length;
-            board[nextPlayer]++;
-            currentPlayerToken--;
+        while (currentPlayerToken>0) { //Is there a better way to do this especially finding the player number
+            nextPlayer=(nextPlayer+1)%board.length; //chooses the player
+            board[nextPlayer]++; //adds token
+            currentPlayerToken--; //subtracts token from tokentotal initial
+
+            /* Logic Behind nextPlayer
+            if we start 6 and do (6+1)%7(this is board length)=0
+            then we do next (0+1)%7=1
+            and so on.
+             */
         }
     }
 
+    public int getCurrentPlayer() { //gets the current player
+        return currentPlayer;
+    }
+
+    public int[] getBoard() { //gets board array(all the tokens)
+        return board;
+    }
+
+    public void setBoard(int[] board) { //method to set the board
+        this.board = board;
+    }
+
+    public void setCurrentPlayer(int currentPlayer) { /*method to make the test work
+     sets the current player*/
+        this.currentPlayer = currentPlayer;
+    }
 }
